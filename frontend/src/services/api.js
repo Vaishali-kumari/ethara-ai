f39eb6e1004d1c8ccd57c24f07d231f32dc5877f
vaishali_kumari <vaishali1559.be23@chitkarauniversity.edu.in>
2026-05-09 15:57:37 +0530

    final fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ethara-ai-production-e7be.up.railway.app/api"
+});
+
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return req;
 });
 
 export default API;
