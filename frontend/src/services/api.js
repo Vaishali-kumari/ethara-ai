0de7393a0585562ba24b3d02de802f84f9d948a5
vaishali_kumari <vaishali1559.be23@chitkarauniversity.edu.in>
2026-05-09 15:18:27 +0530

    fix backend api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://ethara-ai-production-e7be.up.railway.app/api"
 });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
 
   if (token) {
-    req.headers.Authorization = token;
+    req.headers.Authorization = `Bearer ${token}`;
   }
 
   return req;
