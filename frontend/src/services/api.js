1277d32f1aae850681e926e967905216f14211cb
vaishali_kumari <vaishali1559.be23@chitkarauniversity.edu.in>
2026-05-09 15:46:29 +0530

    fix token issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ethara-ai-production-e7be.up.railway.app/api"
-});
-
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return req;
+  baseURL: "http://localhost:5000/api",
 });
 
 export default API;
